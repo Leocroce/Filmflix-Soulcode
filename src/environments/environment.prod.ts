277b1a3cbd36896241fefa80fc319bcf7c57da00
Leocroce <leo_croce@hotmail.com>
2022-01-18 14:40:28 -0300

    Adição da versão inicial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-e7eac',
+    appId: '1:425886779349:web:188b6e59f8efc0a8b59f5c',
+    storageBucket: 'filmflix-e7eac.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDSR_x6mRCSUkK4Emm98hWyDao3CDme61U',
+    authDomain: 'filmflix-e7eac.firebaseapp.com',
+    messagingSenderId: '425886779349',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-e7eac',
+    appId: '1:425886779349:web:188b6e59f8efc0a8b59f5c',
+    storageBucket: 'filmflix-e7eac.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDSR_x6mRCSUkK4Emm98hWyDao3CDme61U',
+    authDomain: 'filmflix-e7eac.firebaseapp.com',
+    messagingSenderId: '425886779349',
+  },
   production: false
 };
 
